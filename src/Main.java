0b94111cbb37e8af6adcd1f4ca900d1d010cfef9
drewspampy22 <drewspampy22@gmail.com>
2022-09-19 20:25:04 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String PartyA = "3";
+
+        if (PartyA == "D")
+        {
+            System.out.println("You're a democrat Donkey!");
+        }
+        else if (PartyA == "R")
+        {
+            System.out.println("You're a Republican Elephant!");
+        }
+        else if (PartyA == "I")
+        {
+            System.out.println("You're a Independent Person!");
+        }
+        else {
+            System.out.println("You're Other!");
+        }
+
+
+
+
+    }
+}
